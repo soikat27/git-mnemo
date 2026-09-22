adc44ced9a57dc5cb0ee04653b3f1b31fb79b979
Soikat <193371447+soikat27@users.noreply.github.com>
2026-09-22 14:28:36 -0400

    feat: implement CardGrid component
    -- fetch json data from public/ – "commands.json"
    -- use Fisher-yates stuffle algorithm and slice 15 commands

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,5 +1,51 @@
+import { useEffect, useState } from "react";
+import GitCard from "./GitCard.jsx";
+
 export default function CardGrid() {
+    const [commands, setCommands] = useState([]);
+
+    async function fetchCommands() {
+        const url = "/commands.json";
+        const response = await fetch(url);
+        if (!response.ok)
+            throw new Error("fetch failed");
+        const data = await response.json();
+
+        const commands = data.map(command => ({
+            command: command.Command, 
+            description: command.Description
+        }));
+        return commands;
+    }
+    function shuffleCommands(commands) {
+        for (let i = commands.length-1; i > 0; i--) {
+            const j = Math.floor(Math.random()*(i+1));
+            [commands[i], commands[j]] = [commands[j], commands[i]];
+        }
+        return commands.slice(0, 15);
+    }
+
+    useEffect(() => {
+        async function getCommands() {
+            try {
+                const allCommands = await fetchCommands();
+                const newCommands = shuffleCommands(allCommands);
+                setCommands(newCommands);
+            }
+            catch(error) {
+                console.log(error);
+                alert("error");
+            }
+        }
+        getCommands(); 
+    }, []);
+    
+
     return (
-        <div></div>
+        <div>
+            {
+                commands.map(command => <GitCard command={command.command} description={command.description} />)
+            }
+        </div>
     );
 }
